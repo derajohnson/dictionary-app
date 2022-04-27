f61ed9ad58471e2dfb3193c9a504830c662bf1dd
johnson chidera <derajohnson64@gmail.com>
2022-04-27 15:28:16 +0100

    completed hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import {Box} from '@chakra-ui/react'
-
+import {Box, Flex, Text, Image, Spacer, Input, Button} from '@chakra-ui/react'
+import girlImg from '../assets/girl.png'
 
 const Home = () => {
     return (
         <Box>
-
+            <Flex flexDirection={{base:'column', md:'row'}}>
+                <Box alignSelf='center' mt={{base: '8', md: '0'}}>
+                    <Text fontSize='3xl' >
+                        Search the meaning of any word. Anyday. Anytime. 
+                    </Text>
+                    <form action="">
+                        <Input placeholder='Search for a word' my='5'/>
+                        <Button bgColor='purple.400' color='white' textAlign={{base: 'center', md: 'left'}}>
+                            Search
+                        </Button>
+                    </form>
+                </Box>
+                <Spacer />
+                <Box>
+                    <Image src={girlImg} alt='girl searching'></Image>
+                </Box>
+            </Flex>
         </Box>
     )
 } 
