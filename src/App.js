530c41f9dc80b96dfbca930162feaad1f978644d
johnson chidera <derajohnson64@gmail.com>
2022-04-28 07:36:07 +0100

    completed testimonials section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Box} from '@chakra-ui/react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar';
 import Content from './components/Content';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero />
       <Content />
+      <Testimonials />
     </Box>
   );
 }
